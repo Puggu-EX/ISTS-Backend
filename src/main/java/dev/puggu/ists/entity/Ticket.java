e7c12433b92235755ba1472a88fbfd464e7caf97
PugguEx <71161000+Puggu-EX@users.noreply.github.com>
2026-04-23 15:03:07 -0700

    New variables, nonnull annotation added

--- src/main/java/dev/puggu/ists/entity/Ticket.java
+++ src/main/java/dev/puggu/ists/entity/Ticket.java
@@ -1,27 +1,48 @@
 package dev.puggu.ists.entity;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
 @Getter @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Ticket {
 
     @Id
     @UuidGenerator
     @Column(name = "ticket_id")
+    @NonNull
     private UUID ticketID;
 
-    private String title;
-    private String description;
-    private String level;
-    private String status;
+    @NonNull
+    private UUID author;
 
-    private String author;
+    @NonNull
+    private String title;
+
+    @NonNull
+    private String description;
+
+    private int escalationLevel; // Also Enum? at least set a min-max
+    private int priority;
+
+    @NonNull
+    private String status; // Enum?
+
+    @NonNull
+    private String tags; // Enum?
+
+    // Required Timeline
+    @NonNull
+    private LocalDate dateSubmitted;
+    private LocalDate dateOpened;
+    private LocalDate dateAddressed;
+    private LocalDate dateSolved;
+
 }
