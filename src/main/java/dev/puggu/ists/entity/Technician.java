d4b1adf5a604f4e663ce2174c54a069f8453afdb
PugguEx <71161000+Puggu-EX@users.noreply.github.com>
2026-04-23 15:05:36 -0700

    Removed duplicate variables already present in superclass

--- src/main/java/dev/puggu/ists/entity/Technician.java
+++ src/main/java/dev/puggu/ists/entity/Technician.java
@@ -6,15 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Entity
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Technician extends User {
-
-    private String firstName;
-    private String lastName;
-
-    private String email;
-    private String password;
+    private int level;
 }
