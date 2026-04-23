601778becd9a7e323f13b6c790346348d96c737a
PugguEx <71161000+Puggu-EX@users.noreply.github.com>
2026-04-23 15:04:39 -0700

    Nonnull annotation added

--- src/main/java/dev/puggu/ists/entity/User.java
+++ src/main/java/dev/puggu/ists/entity/User.java
@@ -3,9 +3,7 @@ package dev.puggu.ists.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
@@ -14,16 +12,22 @@ import java.util.UUID;
 @MappedSuperclass
 @Getter @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public abstract class User {
 
     @Id
     @UuidGenerator
+    @NonNull
     @Column(name = "user_id")
     private UUID userID;
 
+    @NonNull
     private String firstName;
+    @NonNull
     private String lastName;
 
+    @NonNull
     private String email;
+    @NonNull
     private String password;
 }
