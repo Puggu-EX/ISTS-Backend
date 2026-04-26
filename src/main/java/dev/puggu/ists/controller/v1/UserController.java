263c610c4f38e1f0f90ac3ae6e3c1f2f59a927bc
PugguEx <71161000+Puggu-EX@users.noreply.github.com>
2026-04-25 19:05:57 -0700

    Added register and login endpoints

--- src/main/java/dev/puggu/ists/controller/v1/UserController.java
+++ src/main/java/dev/puggu/ists/controller/v1/UserController.java
@@ -1,9 +1,30 @@
 package dev.puggu.ists.controller.v1;
 
+import dev.puggu.ists.controller.dto.request.UserLogin;
+import dev.puggu.ists.controller.dto.request.UserRegister;
+import dev.puggu.ists.service.StandardUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/v1/users")
+@AllArgsConstructor
 public class UserController {
+    public final StandardUserService standardUserService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> createUser(@RequestBody UserRegister userRegisterRequest) {
+        return standardUserService.register(userRegisterRequest);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> createUser(@RequestBody UserLogin userLoginRequest) {
+        return standardUserService.login(userLoginRequest);
+    }
+
+
 }
