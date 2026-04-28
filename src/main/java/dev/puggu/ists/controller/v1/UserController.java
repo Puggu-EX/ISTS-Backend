5c5c8fba8ccd4c252462f6acc4d5737770d367de
PugguEx <71161000+Puggu-EX@users.noreply.github.com>
2026-04-28 11:15:29 -0700

    Relinquished user login to auth endpoint, modified registration endpoint

--- src/main/java/dev/puggu/ists/controller/v1/UserController.java
+++ src/main/java/dev/puggu/ists/controller/v1/UserController.java
@@ -1,31 +1,36 @@
 package dev.puggu.ists.controller.v1;
 
-import dev.puggu.ists.controller.dto.request.UserLogin;
 import dev.puggu.ists.controller.dto.request.UserRegister;
 import dev.puggu.ists.service.StandardUserService;
+import dev.puggu.ists.service.TechnicianService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/v1/users")
+@RestController
 @RequestMapping("api/v1/users")
 @AllArgsConstructor
 public class UserController {
+    //    public final UserService userService; // Generic user services
     public final StandardUserService standardUserService;
+    public final TechnicianService technicianService;
 
-    @PostMapping("/register")
-    public ResponseEntity<?> createUser(@RequestBody UserRegister userRegisterRequest) {
+    @PostMapping
+    public ResponseEntity<?> createUser(@Valid @RequestBody UserRegister userRegisterRequest) {
         return standardUserService.register(userRegisterRequest);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<?> createUser(@RequestBody UserLogin userLoginRequest) {
-        return standardUserService.login(userLoginRequest);
-    }
+//    @GetMapping
+//    public ResponseEntity<?> getUser(@Valid @RequestBody UserRegister userRegisterRequest) {
+//        return standardUserService.register(userRegisterRequest);
+//    }
+
+//    @PatchMapping
+//    public ResponseEntity<?> updateUser(@Valid @RequestBody UserRegister userRegisterRequest) {
+//        return standardUserService.register(userRegisterRequest);
+//    }
+
 
 
 }
