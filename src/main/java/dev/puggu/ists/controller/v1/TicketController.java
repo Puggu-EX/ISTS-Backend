e73944ddf51690642f8b8bceb638318fcb610346
PugguEx <71161000+Puggu-EX@users.noreply.github.com>
2026-04-28 11:12:59 -0700

    Ticket creation endpoint

--- src/main/java/dev/puggu/ists/controller/v1/TicketController.java
+++ src/main/java/dev/puggu/ists/controller/v1/TicketController.java
@@ -1,10 +1,23 @@
 package dev.puggu.ists.controller.v1;
 
+import dev.puggu.ists.controller.dto.request.TicketSubmission;
+import dev.puggu.ists.service.TicketService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("api/v1/tickets")
 @AllArgsConstructor
 public class TicketController {
+    private final TicketService ticketService;
+
+    @PostMapping
+    public ResponseEntity<?> createTicket(@Valid @RequestBody TicketSubmission ticketSubmission) {
+        return ticketService.create(ticketSubmission);
+    }
 }
