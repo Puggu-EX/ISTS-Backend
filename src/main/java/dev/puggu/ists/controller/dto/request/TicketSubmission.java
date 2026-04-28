5dccceb6feb1aaa626f301b738aed15c0493aa57
PugguEx <71161000+Puggu-EX@users.noreply.github.com>
2026-04-28 11:14:24 -0700

    Modified request arguments and priority annotation

--- src/main/java/dev/puggu/ists/controller/dto/request/TicketSubmission.java
+++ src/main/java/dev/puggu/ists/controller/dto/request/TicketSubmission.java
@@ -1,17 +1,12 @@
 package dev.puggu.ists.controller.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-
-import java.time.LocalDate;
-import java.util.UUID;
+import jakarta.validation.constraints.PositiveOrZero;
 
 public record TicketSubmission(
-     @NotBlank UUID author,
+     @NotBlank String author,
      @NotBlank  String title,
      @NotBlank  String description,
-     @NotBlank  int escalation_level,
-     @NotBlank  int priority,
-     @NotBlank  String status,
-     @NotBlank  String tag,
-     @NotBlank  LocalDate date_submitted
+     @PositiveOrZero int priority,
+     String tag
 ) { }
